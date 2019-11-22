7650c083e10efb95bee7a1184a31d84e41154461
Anchello <bob.m@mail.ru>
2019-11-22 16:03:03 +0100

    fix eslint errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,10 @@
-import React, { Component } from "react";
+import React from 'react';
 import './App.scss';
 
-class App extends Component {
-  render() {
-	return (
-	  <div>
-		<h1>My React App!</h1>
-	  </div>
-	);
-  }
-}
+const App = () => (
+  <main>
+    <h1>My React App!</h1>
+  </main>
+);
 
 export default App;
